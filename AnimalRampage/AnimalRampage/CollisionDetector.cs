275e58e99003a0cae9688ac55e7e11af960a12b3
Morgan Garske <morgan.garske@colorado.edu>
2015-04-27 20:54:33 -0600

    will detect collisions between entities

---
+++ AnimalRampage/AnimalRampage/CollisionDetector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace AnimalRampage
+{
+	public class CollisionDetector
+	{
+		public CollisionDetector ()
+		{
+
+		}
+	}
+}
+
